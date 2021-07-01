6b38dbbe99f211178e66f1193d024911e5bd91d5
kenyan-coder <scamsours@gmail.com>
2021-07-01 23:09:56 +0300

    add a record for each entry

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let count = 0;
 let countEl= document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function incrementCount(){
     // console.log("clicked")
-    count = count+1;
+    count +=1;
     countEl.innerText=count;
 }
 // 1. Create a function, save(), which logs out the count when it's called
 function save(){
-    console.log(count)
+    let countstr = count + " - "
+    saveEl.innerText +=countstr;
+    console.log(countstr)
 }
