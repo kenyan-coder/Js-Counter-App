e4904d7514671a631dc78c8711fb6f7af101233e
kenyan-coder <scamsours@gmail.com>
2021-07-01 23:42:45 +0300

    reset the counter to 0 after save

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let count = 0;
 let countEl= document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
-
+//Create an increment function for counting;
 function incrementCount(){
-    // console.log("clicked")
+
     count +=1;
-    countEl.innerText=count;
+    countEl.textContent=count;
 }
 // 1. Create a function, save(), which logs out the count when it's called
 function save(){
     let countstr = count + " - "
-    saveEl.innerText +=countstr;
-    console.log(countstr)
+    saveEl.textContent +=countstr;
+    //Reset the counter to zero after saving;
+    count = 0
+    countEl.textContent=count
 }
